23cea4955ee4b1ad28bcc0c52d314f092370cb19
charliewang <Qili@192.168.1.7>
2013-10-14 21:45:53 +1300

    git

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -4,6 +4,7 @@ public class MyApplication {
 
 	public static void main(String[] args) {
 		
+		System.out.println("GitHub commit try");
 		DataConvert date = new DataConvert();
 		date.start();
 		
