e79f801ca1758e766ac3194f224b03fad65baf8d
charliewang <Qili@192.168.1.7>
2013-10-14 21:48:37 +1300

    second try

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -5,6 +5,7 @@ public class MyApplication {
 	public static void main(String[] args) {
 		
 		System.out.println("GitHub commit try");
+		System.out.println("GitHub commit second try");
 		DataConvert date = new DataConvert();
 		date.start();
 		
